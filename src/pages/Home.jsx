c733520599bddee8c6a0a694aac926cb96853cd5
Dee watson <dumezw3ni@gmail.com>
2025-06-05 09:22:36 +0200

    Browser Router implemented

---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+        <div className="homePage">
+            <p>I am home page</p>
+        </div>
+    )
+}
+
+export default Home;
---
+++ src/pages/Kanban.jsx
@@ -0,0 +1,9 @@
+const Kanban = () => {
+    return (
+        <div className="kanban">
+            <p>I am Kanban Page</p>
+        </div>
+    )
+}
+
+export default Kanban;
